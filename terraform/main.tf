17f20f0dd44dd29ffc3346eca669b951ed1ede5e
Roberto <rodauher@gmail.com>
2022-07-08 08:34:12 +0200

    Implementation Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,6 +21,7 @@ resource "aws_instance" "web" {
   ami                    = var.ami
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.vpc_security_group_ids]
+  key_name = "sshtest"
 
   tags = {
     name = "Terraforminstance-${count.index}"
